06615b9d3d33ee08dcb62e520ce33ffc7452a68a
Jeffrey Schwartz <jefftschwartz@gmail.com>
2023-10-08 18:22:06 -0400

    Allow TokyoNight transparency.

--- init.lua
+++ init.lua
@@ -158,7 +158,7 @@ require('lazy').setup({
     priority = 1000,
     opts = {},
     config = function()
-      require("tokyonight").setup({ style = "moon" })
+      require("tokyonight").setup({ style = "moon", transparent = true })
       vim.cmd.colorscheme 'tokyonight'
     end,
   },

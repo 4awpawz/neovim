e7c6a1d30b3f78a48d708531846f566c2518e21c
Jeffrey Schwartz <jefftschwartz@gmail.com>
2024-03-15 06:52:00 -0400

    Configure lualine to display diagnostics.

--- init.lua
+++ init.lua
@@ -270,6 +270,7 @@ require('lazy').setup({
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     -- See `:help lualine.txt`
     opts = {
       options = {
@@ -289,6 +290,32 @@ require('lazy').setup({
               return ''
             end
           end,
+        },
+        lualine_y = { '%l:%c', '%p%%/%L' },
+        lualine_z = {
+          {
+            'diagnostics',
+
+            -- Table of diagnostic sources, available sources are:
+            --   'nvim_lsp', 'nvim_diagnostic', 'nvim_workspace_diagnostic', 'coc', 'ale', 'vim_lsp'.
+            -- or a function that returns a table as such:
+            --   { error=error_cnt, warn=warn_cnt, info=info_cnt, hint=hint_cnt }
+            sources = { 'nvim_lsp' },
+
+            -- Displays diagnostics for the defined severity types
+            sections = { 'error', 'warn', 'info', 'hint' },
+            diagnostics_color = {
+              -- Same values as the general color option can be used here.
+              error = 'DiagnosticError', -- Changes diagnostics' error color.
+              warn  = 'DiagnosticWarn',  -- Changes diagnostics' warn color.
+              info  = 'DiagnosticInfo',  -- Changes diagnostics' info color.
+              hint  = 'DiagnosticHint',  -- Changes diagnostics' hint color.
+            },
+            symbols = { error = 'E', warn = 'W', info = 'I', hint = 'H' },
+            colored = true,           -- Displays diagnostics status in color if set to true.
+            update_in_insert = false, -- Update diagnostics in insert mode.
+            always_visible = true,    -- Show diagnostics even if there are none.
+          }
         },
       },
     },
